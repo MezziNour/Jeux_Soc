143f3b14aa2b34a56647cddc2a504e2583a25cda
imentrk <imen.triki.itr@gmail.com>
2025-05-26 22:15:20 +0200

    ajout commentaire dans fichier config/db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "imanou",
+  password: "", //ajoutez votre mot de passe MySQL
   database: "app_jeux_soc",
 });
 
